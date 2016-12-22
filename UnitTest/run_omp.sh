7cfee824433c4c8c46a06a172bad6add2315a6aa
lightjameslyy <lightjameslyy@gmail.com>
2016-12-22 14:56:54 +0800

    UnitTest: update run_omp.sh

--- UnitTest/run_omp.sh
+++ UnitTest/run_omp.sh
@@ -6,7 +6,6 @@
 #DATA_ROOT_PATH="/home/liutao/workspace/ygs_data/"
 ./clean_data.sh
 
-
 TEST_PATH="/home/liutao/workspace/github/ygs-omp/UnitTest"
 # run all tests.
 cd ${TEST_PATH}
@@ -19,12 +18,12 @@ export OMP_NUM_THREADS=$1
 
 for curfile in "TestIQCombineMakefile" "TestR2CalibrationMakefile" "TestInterferogramMapsMakefile" "TestRegistrCoarseMakefile" "TestRegistrFineMakefile" "TestPreOrbImportMakefile" "TestRoiSelectProMakefile" "TestFlatPhaseMakefile" "TestCoherenceEstMakefile" "TestComplexProMakefile" "TestMeanCohMakefile" "TestMuskMakefile" "TestBaselineMakefile" "TestPSCMakefile" "TestDeluanayMakefile" "TestIntegrationMakefile" "TestResidueMakefile" "TestResidueUnwMakefile" "TestUonlinearMakefile"
 do
+    sleep 3s 
     echo $curfile >> $logfile
     make -f $curfile 
     /usr/bin/time -p -a -o $logfile ./test
     make -f $curfile clean
     echo "" >> $logfile
-    sleep 10s 
 done
 
 touch "test"
