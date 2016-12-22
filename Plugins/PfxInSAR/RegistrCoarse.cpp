d3e97c8845b28f26d10175f9b0d607161ca669d3
lightjameslyy <lightjameslyy@gmail.com>
2016-12-22 21:33:57 +0800

    Plugins/PfxInSAR: add openmp to RegistrCoarse

--- Plugins/PfxInSAR/RegistrCoarse.cpp
+++ Plugins/PfxInSAR/RegistrCoarse.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <cmath>
 #include <cstdio>
+#include <omp.h>
 
 #define _FILE_OFFSET_BITS 64
 
@@ -47,12 +48,10 @@ void CRegistrCoarse::Coarse(string lpDataFullname1,string lpHdrFullname1,string 
     * 计算得到粗配准偏移量
     */
     //定义主、辅图像
-    //int row1,col1,row2,col2;                              
     double x3d=0,y3d=0,z3d=0;
     image1.LonLat2Coordinate(x3d,y3d,z3d);
 
     //计算主图像行、列号
-    //LocCoordinate(image1,row1,col1,x3d,y3d,z3d);
     CSpMatrix<double> xPolyCoef(4,1);                       //----**aef
     CSpMatrix<double> yPolyCoef(4,1);                       //----**bef
     CSpMatrix<double> zPolyCoef(4,1);                       //----**cef
@@ -80,12 +79,6 @@ void CRegistrCoarse::Coarse(string lpDataFullname1,string lpHdrFullname1,string 
 
     double r=sqrt((satX-x3d)*(satX-x3d)+(satY-y3d)*(satY-y3d)+(satZ-z3d)*(satZ-z3d));                                                   //中间变量
     int colM=(r-image1.m_oHeader.SlantRangeTimeToFirstRangeSample/2*image1.CONST_MATH_LIGHT_VELOCITY)/image1.m_oHeader.SampledPixelSpacing;
-
-    //计算辅图像行、列号
-    //LocCoordinate(image2,row2,col2,x3d,y3d,z3d);  
-    //CSpMatrix<double> xPolyCoef(4,1);                     //----**aef
-    //CSpMatrix<double> yPolyCoef(4,1);                     //----**bef
-    //CSpMatrix<double> zPolyCoef(4,1);                     //----**cef
 
     image2.OrbitCoef(image2.m_oHeader.StateVector,xPolyCoef,yPolyCoef,zPolyCoef);
 
@@ -150,6 +143,7 @@ void CRegistrCoarse::Coarse(string lpDataFullname1,string lpHdrFullname1,string 
             {
                 fseeko(fp1,(sRow*image2.m_oHeader.Sample)*sizeof(complex<INT16>),SEEK_SET);
                 fread(data,sizeof(complex<INT16>),image2.m_oHeader.Sample,fp1);
+#pragma omp parallel for
                 for(int j=0;j<image1.m_oHeader.Sample;j++)
                 {
                     int colS = j + colOffset;
@@ -178,6 +172,7 @@ void CRegistrCoarse::Coarse(string lpDataFullname1,string lpHdrFullname1,string 
             {
                 fseeko(fp1,(sRow*image2.m_oHeader.Sample)*sizeof(complex<float>),SEEK_SET);
                 fread(data,sizeof(complex<float>),image2.m_oHeader.Sample,fp1);
+#pragma omp parallel for
                 for(int j=0;j<image1.m_oHeader.Sample;j++)
                 {
                     int colS = j + colOffset;
@@ -206,23 +201,31 @@ void CRegistrCoarse::Coarse(string lpDataFullname1,string lpHdrFullname1,string 
     header.Save(lpHdrExport);
 }
 
-
-
+/*
+ * add openmp by liutao
+ * resize blockBuffer[] to blockWidth*blockHeight 
+ * step1: I/O to read to blockBuffer[]
+ * step2: parallel generate block[]
+ */
 float* CRegistrCoarse::CoarseBlock(CRMGImage &image,int blockWidth,int blockHeight,Rect window)
 {
     float *block=new float[blockWidth*blockHeight];                 // 子区域数据
-    complex<INT16>* blockBuffer=new complex<INT16>[blockWidth];     //子区一行数据
+    complex<INT16>* blockBuffer=new complex<INT16>[blockWidth*blockHeight];     //子区一行数据
     
     FILE* fp1=fopen(image.m_lpFullname.c_str(),"rb+");
     long offset=(window.top*image.m_oHeader.Sample+window.left)*sizeof(complex<INT16>);
     fseek(fp1,offset,SEEK_SET);
-    //读取图像子区域数据
+    // read sub-region data to blockBuffer[]
     for(int i=0;i<blockHeight;i++)
     {       
-        fread(blockBuffer, sizeof(complex<INT16>),blockWidth,fp1);
-        for(int j=0;j<blockWidth;j++)
-            block[i*blockWidth+j]=sqrtf(blockBuffer[j].real()*blockBuffer[j].real()+blockBuffer[j].imag()*blockBuffer[j].imag());       
+        fread(blockBuffer+i*blockWidth, sizeof(complex<INT16>),blockWidth,fp1);
         fseek(fp1,(image.m_oHeader.Sample-blockWidth)*sizeof(complex<INT16>),SEEK_CUR);
+    }//for i
+    // generate data to block[]
+#pragma omp parallel for
+    for(int i=0;i<blockHeight*blockWidth;i++)
+    {       
+        block[i]=sqrtf(blockBuffer[i].real()*blockBuffer[i].real()+blockBuffer[i].imag()*blockBuffer[i].imag());       
     }//for i
     delete[] blockBuffer;
     return  block;
@@ -230,15 +233,6 @@ float* CRegistrCoarse::CoarseBlock(CRMGImage &image,int blockWidth,int blockHeig
 
 void CRegistrCoarse::CoarseOffset(CRMGImage &image1,CRMGImage &image2,int &colOffset,int &rowOffset)
 {
-    //int blockWidth=1000;                                                                  //子区域宽度
-    //int blockHeight=2000;                                                                 //子区域高度
-    
-    //double x3d=0,y3d=0,z3d=0;
-    //image1.LonLat2Coordinate(x3d,y3d,z3d);
-
-//  float *block1=CoarseBlock(image1,blockWidth,blockHeight,0,0,rect);                      //主图像 子区域数据
-//  float *block2=CoarseBlock(image2,blockWidth,blockHeight,rowOffset,colOffset,rect);      //辅图像 子区域数据
-
     int gridRno = int(sqrtf(100*image1.m_oHeader.Line/image1.m_oHeader.Sample));        //行上窗口个数
     int gridCno = int(100/gridRno);                                                 //列上窗口个数
     int gridNo   = gridCno*gridRno;                                 //网格总个数
@@ -249,33 +243,28 @@ void CRegistrCoarse::CoarseOffset(CRMGImage &image1,CRMGImage &image2,int &colOf
     int gridAzimuthSpace=(image1.m_oHeader.Line -2*(gridSize+gridSearchRange+100)-2*abs(rowOffset))/gridRno;        //网格方位向距离
     const int gridBoyndaryOffset_C = (gridSize+gridSearchRange+100) + abs(colOffset); 
     const int gridBoyndaryOffset_R = (gridSize+gridSearchRange+100) + abs(rowOffset);
-    // CPoint gridCenter;                                              //网格中心点
-    Point gridCenter;
-    int gridCount=0;                                                //已计算的网格数量
+    //Point gridCenter;
     int *gridOffsetX = new int[gridNo];                         //距离偏移
     int *gridOffsetY = new int[gridNo];                         //方位偏移
     double *gridMaxCoefAry = new double[gridNo];                        //最大相关系数组
-    //初始化
     memset(gridMaxCoefAry,0,sizeof(double)*gridNo);     
-    float gridCoef=0,gridMaxCoef=0;                                                 //相关系数
 
-    float *grid1=new float[gridSize*gridSize];                                      //主网格数据
-    float *grid2=new float[(gridSize+2*gridSearchRange+1)*(gridSize+2*gridSearchRange+1)];  //辅网格搜索窗口大小的数据
-    float *grid22 = new float[gridSize*gridSize];                   //辅图像对应的匹配窗口大小的数据
-
+    omp_lock_t img1_lock, img2_lock;
+    omp_init_lock(&img1_lock);
+    omp_init_lock(&img2_lock);
     
-    //float stSum=0,stVar=0,stStdev,stSqSum=0,stMean=0;             //统计变量定义
-    //float stSqMean=0;
-    CSpStatistics<float> spStat;                                    //统计对象
-    
-    float tmpValue=0;                                               //临时中间变量
-
-    // CRect dataWin1,dataWin2;
-    Rect dataWin1, dataWin2;
+#pragma omp parallel for collapse(2)
     for(int i=0;i<gridRno;i++)                                          //窗口循环
     {
         for(int j=0;j<gridCno;j++)
         {
+            Rect dataWin1, dataWin2;
+            float *grid1=new float[gridSize*gridSize];                                      //主网格数据
+            float *grid2=new float[(gridSize+2*gridSearchRange+1)*(gridSize+2*gridSearchRange+1)];  //辅网格搜索窗口大小的数据
+            float *grid22 = new float[gridSize*gridSize];                   //辅图像对应的匹配窗口大小的数据
+            CSpStatistics<float> spStat;                                    //统计对象
+            float gridCoef=0,gridMaxCoef=0;                                                 //相关系数
+
             dataWin1.SetRect(gridBoyndaryOffset_C+i*gridRangeSpace,gridBoyndaryOffset_R+j*gridAzimuthSpace,
                             gridBoyndaryOffset_C+i*gridRangeSpace+gridSize-1,gridBoyndaryOffset_R+j*gridAzimuthSpace+gridSize-1);
 
@@ -283,8 +272,13 @@ void CRegistrCoarse::CoarseOffset(CRMGImage &image1,CRMGImage &image2,int &colOf
                 gridBoyndaryOffset_C+i*gridRangeSpace+colOffset+gridSize+gridSearchRange,
                 gridBoyndaryOffset_R+j*gridAzimuthSpace+rowOffset+gridSize+gridSearchRange);
             
+            // CoarseBlock contains I/O operations, to avoid disk pointer busy, make it a critical section
+            omp_set_lock(&img1_lock);
             grid1 = CoarseBlock(image1,gridSize,gridSize,dataWin1);
+            omp_unset_lock(&img1_lock);
+            omp_set_lock(&img2_lock);
             grid2 = CoarseBlock(image2,gridSize+2*gridSearchRange+1,gridSize+2*gridSearchRange+1,dataWin2);
+            omp_unset_lock(&img2_lock);
 
         
             //寻找 辅图像 最大求相关系数
@@ -297,34 +291,42 @@ void CRegistrCoarse::CoarseOffset(CRMGImage &image1,CRMGImage &image2,int &colOf
                     {
                         for(int jjj=0;jjj<gridSize;jjj++)
                         {
-                            tmpValue=grid2[(ii+iii)*(gridSize+gridSearchRange)+(jj+jjj)];
-                            grid22[iii*gridSize+jjj]=tmpValue;
+                            grid22[iii*gridSize+jjj]=grid2[(ii+iii)*(gridSize+gridSearchRange)+(jj+jjj)];
                         }//inner for jjj
                     }//inner for iii
+
                     //计算两网格统计值 及其 协方差
                     spStat.Set(grid1,gridSize*gridSize,grid22);
                     spStat.DoAdvanced();
                     
                     //计算相关系数 --这里采用了统计对象  如严重影响效率可单独写统计代码
                     gridCoef=spStat.m_xVar*spStat.m_yVar==0?0:fabs(spStat.m_Covariance)/sqrt(spStat.m_xVar*spStat.m_yVar);
-                    gridOffsetX[gridCount]=0;
-                    gridOffsetY[gridCount]=0;
+                    gridOffsetX[i*gridCno+j]=0;
+                    gridOffsetY[i*gridCno+j]=0;
                     if(gridMaxCoef<gridCoef) 
                     {
                         gridMaxCoef=gridCoef;
-                        gridOffsetX[gridCount]=jj - gridSearchRange;
-                        gridOffsetY[gridCount]=ii - gridSearchRange;
+                        gridOffsetX[i*gridCno+j]=jj - gridSearchRange;
+                        gridOffsetY[i*gridCno+j]=ii - gridSearchRange;
                     }//if
                 }//for jj
             }//for ii
-            gridMaxCoefAry[gridCount]=gridMaxCoef;//循环开始不用初始化？
-            gridCount++;
+            gridMaxCoefAry[i*gridCno+j]=gridMaxCoef;//循环开始不用初始化？
+
+            delete[] grid1;
+            delete[] grid2;
+            delete[] grid22;
         }//for j
     }//for i
+
+    omp_destroy_lock(&img1_lock);
+    omp_destroy_lock(&img2_lock);
+
     CSpStatistics<double> spStatCoef(gridMaxCoefAry,gridNo);                            //最大相关系数统计量
     int stLength=0;                                                                             //统计量  有效个数
     float stMean=0,stStdevX=0,stStdevY=0,stSumX=0,stSumY=0,stMeanX=0,stMeanY=0;
 
+#pragma omp parallel for reduction(+: stSumX, stSumY, stStdevX, stStdevY, stLength)
     for(int i=0;i<gridNo;i++)
     {
         if(gridMaxCoefAry[i]>=spStatCoef.m_xMean)
@@ -336,27 +338,10 @@ void CRegistrCoarse::CoarseOffset(CRMGImage &image1,CRMGImage &image2,int &colOf
             stLength++;
         }//if
     }//for i
+
     stMeanX=stSumX/stLength;
     stMeanY=stSumY/stLength;
-    //stStdevX=sqrt(stStdevX-stMeanX*stMeanX);
-    //stStdevY=sqrt(stStdevY-stMeanY*stMeanY);
 
-    /*stSumX=0;
-    stSumY=0;
-    stLength=0;
-    for(int i=0;i<gridBoundaryOffset;i++)
-    {
-        if(gridMaxCoefAry[i]>=spStat.m_xMean
-         &&abs(gridOffsetX[i]-stMeanX)<=stStdevX
-         &&abs(gridOffsetY[i]-stMeanY)<=stStdevY)
-        {
-            stSumX+=gridOffsetX[i];
-            stSumY+=gridOffsetY[i];
-            stLength++;
-        }//if
-    }//for i
-    stMeanX=stSumX/stLength;
-    stMeanY=stSumY/stLength;*/
     //返回偏移参数
     colOffset+=int(stMeanX);
     rowOffset+=int(stMeanY);
@@ -364,96 +349,4 @@ void CRegistrCoarse::CoarseOffset(CRMGImage &image1,CRMGImage &image2,int &colOf
     delete[] gridOffsetX;
     delete[] gridOffsetY;
     delete[] gridMaxCoefAry;
-    delete[] grid1;
-    delete[] grid2;
-    delete[] grid22;
 }
-
-/*DOUBLE CRegistrCoarse::LocCoordinate(CRMGImage &image,int &row,int &col,double x3d,double y3d,double z3d)
-{
-    //图像轨道拟合系数  
-    CSpMatrix<double> xPolyCoef(4,1);                       //----**aef
-    CSpMatrix<double> yPolyCoef(4,1);                       //----**bef
-    CSpMatrix<double> zPolyCoef(4,1);                       //----**cef
-    
-    image.OrbitCoef(image.m_oHeader.StateVector,xPolyCoef,yPolyCoef,zPolyCoef);
-
-    //double x3d,y3d,z3d;                                           //中心坐标，主、辅图像都用该坐标
-    //经纬度转换到三维坐标
-    //image.LonLat2Coordinate(x3d,y3d,z3d);
-
-    double timeSpan=image.m_oHeader.StateVector[1].timePoint-image.m_oHeader.StateVector[0].timePoint;
-    double ft1= 0;
-    //double ft2=(image.CONST_ORBIT_SAPMLING_POINT_COUNT-1)*timeSpan+5; //时间间隔
-    double ft2=(image.m_oHeader.StateVector.size()-1)*timeSpan+5;   //时间间隔
-    double ft3=(ft1+ft2)/2; 
-    
-    while(fabs(ft1-ft2)>1e-6)
-    {   
-        
-        //计算多项式值
-        double p1=Polynomial(xPolyCoef,yPolyCoef,zPolyCoef,ft1,x3d,y3d,z3d);
-        double p2=Polynomial(xPolyCoef,yPolyCoef,zPolyCoef,ft2,x3d,y3d,z3d);
-        double p3=Polynomial(xPolyCoef,yPolyCoef,zPolyCoef,ft3,x3d,y3d,z3d);
-    
-        if(p1*p3>=0&&p2*p3<=0) {ft1=ft3;ft3=(ft1+ft2)/2;}
-        else if(p1*p3<=0&&p2*p3>=0)  {ft2=ft3;ft3=(ft1+ft2)/2;}
-        else 
-        {
-            AfxMessageBox("Polyfit Fail!");
-            exit(0);
-        }
-    }//while fabs
-    double timeAzimuth= ft3;    
-    //计算图像对应坐标
-    double x=Polyfit(xPolyCoef,timeAzimuth,0);          
-    double y=Polyfit(yPolyCoef,timeAzimuth,0);          
-    double z=Polyfit(zPolyCoef,timeAzimuth,0);
-    if(!image.m_oHeader.PassDirection)  //方位向时间
-        timeAzimuth+=(image.m_oHeader.StateVector[0].timePoint-image.m_oHeader.ZeroDopplerTimeLastLine);//升轨
-    else
-        timeAzimuth+=(image.m_oHeader.StateVector[0].timePoint-image.m_oHeader.ZeroDopplerTimeFirstLine);//升轨
-
-    row= int(timeAzimuth*image.m_oHeader.PulseRepetitionFrequency);                 //int(timeAzimuth*timeSpan);
-
-    double r=sqrt((x-x3d)*(x-x3d)+(y-y3d)*(y-y3d)+(z-z3d)*(z-z3d));                                                 //中间变量
-    col=(r-image.m_oHeader.SlantRangeTimeToFirstRangeSample/2*image.CONST_MATH_LIGHT_VELOCITY)/image.m_oHeader.SampledPixelSpacing;
-}
-*/
-
-
-// #pragma region Poly多项式拟合
-/*DOUBLE CRegistrCoarse::Polynomial(CSpMatrix<double> &aef,CSpMatrix<double> &bef,CSpMatrix<double> &cef,double t,double x,double y,double z)
-{   
-    return  (Polyfit(aef,t,0)-x)*Polyfit(aef,t,1)+                  //(X(t)-x0)*Vx(t)
-            (Polyfit(bef,t,0)-y)*Polyfit(bef,t,1)+                  //(Y(t)-y0)*Vy(t)
-            (Polyfit(cef,t,0)-z)*Polyfit(cef,t,1);                  //(Z(t)-z0)*Vz(t)
-}
-DOUBLE CRegistrCoarse::Polyfit(CSpMatrix<double> &mat,double t,int order)
-{
-    double value=0,item=0,coefficient=1;
-    if(order==0) 
-        value=mat.Get(0,0)+mat.Get(1,0)*t+mat.Get(2,0)*t*t+mat.Get(3,0)*t*t*t;
-    else if(order==1) 
-        value=mat.Get(1,0)+ mat.Get(2,0)*t*2 + mat.Get(3,0)*t*t*3;  
-
-    //for(int i=0;i<=3;i++)
-    //{
-    //  //以下两项计算多项式导数
-    //  //t求导后多次幂
-    //  item=(i-order)>=0?pow(t,i-order):0;         
-    //  //导数系数
-    //  coefficient=PolyCoef(i,order);
-    //  //计算多项式值
-    //  value+=mat.Get(i,0)*coefficient*item;
-    //}
-    return value;
-}
-LONG CRegistrCoarse::PolyCoef(int n,int order)
-{
-    long value=1;
-    for(int i=0;i<order;i++)
-        value*=n-i;     
-    return value>0?value:1; 
-}*/
-// #pragma endregion
